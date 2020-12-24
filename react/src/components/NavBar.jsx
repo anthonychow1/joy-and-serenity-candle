fdeeab3591a1219fbdb52889a83c45a3e6477527
Anthony Chow <anthony.c.chow@gmail.com>
2020-12-24 01:23:37 -0800

    Added Logos, NavBar, Fonts, and Router

---
+++ react/src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+//import PropTypes from "prop-types";
+
+//const [isLogged, setIsLogged] = useState(false);
+
+const NavBar = () => {
+
+    const getPublicRoutes = () => {
+        return (
+            <ul className="public-navbar-list">
+                <li className="public-navbar-item">
+                    <NavLink to={"/about"} className="public-navbar-link">
+                        About
+                    </NavLink>
+                </li>
+                <li className="public-navbar-item">
+                    <NavLink to={"/products"} className="public-navbar-link">
+                        Products
+                    </NavLink>
+                </li>
+                <li className="public-navbar-item">
+                    <NavLink to={"/blogs"} className="public-navbar-link">
+                        Blogs
+                    </NavLink>
+                </li>
+                <li className="public-navbar-item">
+                    <NavLink to={"/contact"} className="public-navbar-link">
+                        Contact Us
+                    </NavLink>
+                </li>
+                <li className="public-navbar-item">
+                    <NavLink to={"/login"} className="public-navbar-link">
+                        Login
+                    </NavLink>
+                </li>
+            </ul>
+            );
+    }
+
+    //return isLogged ? getPrivateRoutes(""):  getPublicRoutes();
+    return getPublicRoutes();
+
+}
+
+export default NavBar;
