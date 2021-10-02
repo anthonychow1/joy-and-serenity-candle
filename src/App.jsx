00766b547ab0977b2263a14fc7e109dbcc3e3ce5
Anthony <anthony.c.chow@gmail.com>
2021-10-01 19:30:41 -0700

    covert to Soleil

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Route, withRouter } from "react-router-dom";
 import About from "./About";
 import './App.css';
 import NavBar from "./components/navbars/NavBar";
-// import ProductList from "./components/sections/ProductList"
+import Header from "./components/sections/Header";
+import ProductList from "./components/sections/ProductList"
 import Section from "./components/sections/Section"
 
 class App extends React.Component {
@@ -11,9 +12,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="app-container">
-                <header>
-                    <h1>Joy&nbsp;and&nbsp;Serenity&nbsp;Candle</h1>
-                </header>
+                <Header />
                 <hr />
                 <div className="public-nav">
                     <nav>
@@ -23,13 +22,14 @@ class App extends React.Component {
                 <hr />
                 <main>
                     <Route path="/" exact={true}>
-                        <Section cssName="succulent" content={`Hello`}/>
+                        <Section cssName="succulent" content={`Soleil`}/>
                         <hr />
-                        <Section cssName="wood" content={`Hello`}/>
+                        <Section cssName="wood" content={`Soleil`}/>
                     </Route>
                     <Route path="/about" exact={true} component={About} />
+                    <Route path="/products" exact={true} component={ProductList} />
                 </main>
-                <footer> Joy and Serenity Candle &copy; 2020 </footer>
+                <footer> Soleil &copy; 2021 </footer>
             </div>
         );
     }
---
+++ src/components/sections/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+        <h1>Soleil</h1>
+        <h2>CANDLE + HOME CO</h2>
+        </header>
+    );
+}
+export default Header;
